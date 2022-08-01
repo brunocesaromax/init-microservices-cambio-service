b6719bc15d668c3462651c822c9235054131d6ca
Bruno César <bruno.cesar@cilia.com.br>
2022-08-01 12:09:56 -0300

    Add exchange implementation and move queue creation to consumers

--- src/main/java/com/microservices/cambioservice/configuration/BookCreatedListener.java
+++ src/main/java/com/microservices/cambioservice/configuration/BookCreatedListener.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class BookCreatedListener {
 
-    @RabbitListener(queues = "book-service.v1.create-book")
+    @RabbitListener(queues = "book-service.v1.create-book.cambio-service")
     public void onBookCreated(BookRetrieve bookRetrieve) {
         System.out.println(bookRetrieve.toString());
     }
